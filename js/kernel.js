47c84da4bd8fa4f7a0c8a014d0d688ab655fe9d3
Addisonbean <addisonbean@gmail.com>
2013-05-30 21:34:57 -0500

    Added more examples

--- js/kernel.js
+++ js/kernel.js
@@ -1,12 +1,21 @@
 window.Kernel = (function() {
 	"use strict"
 
+	// declare private var like this:
+	// var secret = "BLABLABLA";
+	// so the var secret couldnt be acess by doing: k.secret
+
 	var Kernel = function() {
-		
+		this.version = 0.01;
 	}
 
+	// define methods like this
 	Kernel.prototype.ls = function (argument) {
 		console.log('this is the file system');
+	}
+
+	Kernel.prototype.info = function (argument) {
+		console.log('Version ' + this.version.toString() + ' Alpha');
 	}
 
 	return Kernel
