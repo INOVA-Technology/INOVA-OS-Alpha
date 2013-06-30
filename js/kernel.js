a5948f9e26aa2dc6a67c4c8b71545aff469da0e9
Addisonbean <addisonbean@gmail.com>
2013-06-29 20:34:21 -0500

    Refactored LOTS of stuff

--- js/kernel.js
+++ js/kernel.js
@@ -19,7 +19,6 @@ window.Kernel = (function() {
 		
 		this.currentUsr = localStorage.username || this.username[0];
 		this.$PS1 = "";
-		this.setprompt("[\\u]$");
 		this.files = localStorage.system ? JSON.parse(localStorage.system) : {"/": {
 			"Users/": {
 				"Johny/": {
@@ -85,8 +84,172 @@ window.Kernel = (function() {
 			}
 		}}; 
 		this.dir = this.files["/"];
+		self = this;
+		this.commands = { // these are the functions meant to be run as commands
+						  // in these, use self instead of this in these functions
+			setprompt: function(p) {
+				self.$PS1 = p;
+			},
+
+			cat: function(file) {
+				if (file !== undefined) {
+					self.stdout(self.dir[file].content + "<br/>");
+				} else {
+		    		self.stdout("please include a file name");
+		    	}
+		    },
+
+			clear: function(text) {
+				self.output.innerHTML = "";
+			},
+
+			rm: function(file) {
+				if (file != "Users/" && file != "bin/" && file != "usr/") {
+					delete self.dir[file];
+					self.stdout(file + " has been deleted");
+				}
+				else {
+					self.stdout("you do not have permissions to delete this folder");
+				}
+			},
+
+			ls: function (file) {
+				if (file) {
+					for(var key in self.dir[file]) {
+					    self.stdout(key + "<br/>");
+					}
+				}
+				else {
+					for(var key in self.dir) {
+					    self.stdout(key + "<br/>");
+					}
+				}
+			},
+
+			login: function(guy, pass) {
+				if (self.username.contains(guy)) {
+					if (pass == self.password[guy]["password"]) {
+						self.currentUsr = guy;
+						self.stdout("logged in as " + self.currentUsr);
+					}
+					else {
+						self.stdout("wrong password");
+					}
+		        }
+		        else {
+		        	self.stdout("Not a real user");
+			    }
+			    self.stdout("<br/>")
+			},
+
+			write: function(file, text) {
+				self.dir[file] = {};
+				self.dir[file].content = '"' + Array.prototype.slice.call(arguments).join(" ") + '"';
+			},
+
+			newUsr: function(name, pass) {
+				self.username.push(name);
+				self.files["/"]["Users/"][name + "/"] = {};
+				self.password[name] = {
+					"password": pass
+				};
+				self.stdout("New user: " + name);
+			}, 
+
+			usrList: function() {
+				self.stdout("Users: <br />" + self.username + "<br />");
+			}, 
+
+			whoami: function(display) {
+				self.stdout(self.currentUsr + "<br/>");
+			},
+
+			info: function () {
+				self.stdout('Version ' + self.version.toString() + ' Alpha<br/>');
+			},
+
+			cd: function (path) {
+				var i = path;
+
+				if (i == "~") {
+					self.dir = self.files["/"];
+				}
+				else {
+					if (self.dir[path]) {
+						self.dir = self.dir[path];
+					}
+					else {
+						self.stdout('cd: ' + path + ': No such file or directory');
+					}
+				}
+			},
+
+			help: function() {
+				self.stdout("COMMANDS: <br />");
+				self.stdout("ls<br />");
+				self.stdout("cd<br />");
+				self.stdout("info<br />");
+				self.stdout("help<br />");
+				self.stdout("fun<br />");
+				self.stdout("clear<br />");
+				self.stdout("cat<br />");
+				self.stdout("whoami<br />");
+				self.stdout("login<br />");
+				self.stdout("newUsr<br />");
+				self.stdout("usrList<br />");
+				self.stdout("rm<br />");
+			},
+
+			touch: function(file) {
+				if (file == undefined) {
+					self.stdout("please enter a file name <br />");
+				}
+				else {
+					self.dir[file] = {
+						content: ""
+					};
+				}
+			},
+
+			fun: function (text, times) {
+				var i = 1;
+				times = parseInt(times, 10);
+				while (i <= times) {
+					self.stdout(text + " <br />");
+					i++;
+				}
+				self.stdout('<br/>');
+			},
+
+			man: function(cmd) {
+				var out;
+				try {
+					out = this.files["/"]["bin/"][cmd]["man"];
+				} catch(e) {
+					try {
+						out = this.files["/"]["usr/"]["bin/"][cmd]["man"];
+					} catch(e) {
+						out = "Man: Command '" + cmd + "' not found";
+					}
+				}
+				this.stdout(out + "<br/>");
+			},
+
+			mkdir: function(file) {
+				if (file == undefined) {
+					this.stdout("please enter a folder name <br />");
+				}
+				else {
+					this.dir[file + "/"] = {};
+				}
+			}
+		};
+
+		this.commands.setprompt("[\\u]$");
+
 	};
 
+	// functions not meant to be run as commands go here
 	Array.prototype.contains = function(obj) {
 		var i, l = this.length;
 		for (i = 0; i < l; i++) {
@@ -97,13 +260,12 @@ window.Kernel = (function() {
 		return false;
 	};
 
-	Kernel.prototype.setprompt = function(p) {
-		this.$PS1 = p;
-		// this.$PS1 = p.replace(/\\/, "\\\\");
+	Kernel.prototype.whoami = function() {
+		return this.currentUsr;
 	}
 
 	Kernel.prototype.getprompt = function() {
-		return k.$PS1.replace(/\\u/, k.currentUsr);
+		return k.$PS1.replace(/\\u/, this.currentUsr);
 		// backslashes must be escaped
 		// in regex and strings
 		// so \\\\ is really \\
@@ -114,182 +276,16 @@ window.Kernel = (function() {
 		this.output.innerHTML += text.toString();
 	};
 
-	Kernel.prototype.clear = function(text) {
-		this.output.innerHTML = "";
-	};
-
-	Kernel.prototype.rm = function(file) {
-		if (file != "Users/" && file != "bin/" && file != "usr/") {
-			delete this.dir[file];
-			this.stdout(file + " has been deleted");
-		}
-		else {
-			this.stdout("you do not have permissions to delete this folder");
-		}
-	}
-
-	Kernel.prototype.cat = function(file) {
-		if (file !== undefined) {
-			this.stdout(this.dir[file].content + "<br/>");
-		} else {
-    		this.stdout("please include a file name");
-    	}
-    };
-
-	Kernel.prototype.login = function(guy, pass) {
-		if (this.username.contains(guy)) {
-			if (pass == this.password[guy]["password"]) {
-				this.currentUsr = guy;
-				this.stdout("logged in as " + this.currentUsr);
-			}
-			else {
-				this.stdout("wrong password");
-			}
-        }
-        else {
-        	this.stdout("Not a real user");
-	    }
-	    this.stdout("<br/>")
-	};
-
-
-	Kernel.prototype.write = function(file, text) {
-		this.dir[file] = {};
-		this.dir[file].content = '"' + Array.prototype.slice.call(arguments).join(" ") + '"';
-	};
-
-	Kernel.prototype.newUsr = function(name, pass) {
-		this.username.push(name);
-		this.files["/"]["Users/"][name + "/"] = {};
-		this.password[name] = {
-			"password": pass
-		};
-		this.stdout("New user: " + name);
-	};
-
-	Kernel.prototype.usrList = function() {
-		this.stdout("Users: <br />" + this.username + "<br />");
-	}
-
-	Kernel.prototype.whoami = function(display) {
-		display = typeof display !== 'undefined' ? display : true;
-		if (display) {
-			this.stdout(this.currentUsr + "<br/>");
-		} else {
-			return this.currentUsr;
-		}
-	};
-
 	Kernel.prototype.runCommand = function(cmd) {
 		this.stdout(this.getprompt() + " " + cmd.toString() + "<br/><br/>");
 		var args = cmd.split(" ").slice(1);
 		var cmd = cmd.split(" ")[0];
-		if (this.__proto__[cmd]) {
-			this[cmd].apply(this, args);
+		if (this.commands[cmd]) {
+			this.commands[cmd].apply(this, args);
 		} else {
 			this.stdout("Unknown command " + '"' +cmd.split(" ")[0] + '"<br/>');
 		}
 		this.stdout("<br/>");
-	};
-
-	Kernel.prototype.ls = function (file) {
-		if (file) {
-			for(var key in this.dir[file]) {
-			    this.stdout(key + "<br/>");
-			}
-		}
-		else {
-			for(var key in this.dir) {
-			    this.stdout(key + "<br/>");
-			}
-		}
-	};
-
-	Kernel.prototype.info = function () {
-		this.stdout('Version ' + this.version.toString() + ' Alpha<br/>');
-	};
-
-	Kernel.prototype.cd = function (path) {
-		var i = path;
-
-		if (i == "~") {
-			this.dir = this.files["/"];
-		}
-		else {
-			if (this.dir[path]) {
-				this.dir = this.dir[path];
-			}
-			else {
-				this.stdout('cd: ' + path + ': No such file or directory');
-			}
-		}
-	};
-
-	Kernel.prototype.help = function() {
-		this.stdout("COMMANDS: <br />");
-		this.stdout("ls<br />");
-		this.stdout("cd<br />");
-		this.stdout("info<br />");
-		this.stdout("help<br />");
-		this.stdout("fun<br />");
-		this.stdout("clear<br />");
-		this.stdout("cat<br />");
-		this.stdout("whoami<br />");
-		this.stdout("login<br />");
-		this.stdout("newUsr<br />");
-		this.stdout("usrList<br />");
-		this.stdout("rm<br />");
-	};
-
-	Kernel.prototype.fun = function (text, times) {
-		var i = 1;
-		times = parseInt(times, 10);
-		while (i <= times) {
-			this.stdout(text + " <br />");
-			i++
-		}
-		this.stdout('<br/>');
-	};
-
-	Kernel.prototype.man = function(cmd) {
-		var out;
-		try {
-			out = this.files["/"]["bin/"][cmd]["man"];
-		} catch(e) {
-			try {
-				out = this.files["/"]["usr/"]["bin/"][cmd]["man"];
-			} catch(e) {
-				out = "Man: Command '" + cmd + "' not found";
-			}
-		}
-		this.stdout(out + "<br/>");
-	};
-
-	Kernel.prototype.mkdir = function(file) {
-		if (file == undefined) {
-			this.stdout("please enter a folder name <br />");
-		}
-		else {
-			this.dir[file + "/"] = {};
-		}
-	};
-
-	Kernel.prototype.touch = function(file) {
-		if (file == undefined) {
-			this.stdout("please enter a file name <br />");
-		}
-		else {
-			this.dir[file] = {
-				content: ""
-			};
-		}
-
-	};
-
-	Kernel.prototype.pwd = function() {
-
-		// this will show the path, like: /Users/Guest/
-		// or like /usr/local/bin/
 	};
 
 	return Kernel;
